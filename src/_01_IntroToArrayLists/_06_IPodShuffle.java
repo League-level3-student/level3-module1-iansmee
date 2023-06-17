b95ef844ae01b27260de96019ebd2b7ca603e6bc
league <league@imac-14.attlocal.net>
2023-06-17 14:26:59 -0700

    Finished Ipod Shuffle

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,17 +1,23 @@
 package _01_IntroToArrayLists;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 //Copyright The League of Amazing Programmers, 2015
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener{
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
 			Song song = new Song("demo.mp3");
-				song.play();
-				song.stop();
+
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -19,10 +25,60 @@ public class _06_IPodShuffle{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
+				
+	}
+	JFrame frame = new JFrame();
+			JPanel panel = new JPanel();
+			JButton button = new JButton();
+			ArrayList<Song> songs = new ArrayList<Song>();
+	Random ran = new Random();
+	
+	Song current;
+	
+	void surprise() {
+		
+		Song everywhere = new Song("Garfields.mp3");
+		Song vampire = new Song("PopBot.mp3");
+		Song free = new Song("Standoff.mp3");
+			
+			current = free;
+			
+			songs.add(everywhere);
+			songs.add(vampire);
+			songs.add(free);
+			
+			
+			
+			panel.add(button);
+			frame.add(panel);
+			button.addActionListener(this);
+			button.setText("Surprise Me!");
+			frame.setVisible(true);
+			frame.pack();
+			
+			
 		
 	}
 	
+	
 	public static void main(String[] args) {
-		new _06_IPodShuffle();
+		_06_IPodShuffle IPodShuffle = new _06_IPodShuffle();
+		
+		IPodShuffle.surprise();
+		
+		
+		
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		current.stop();
+		int x = ran.nextInt(3);
+		current = songs.get(x);
+		current.play();
 	}
 }
